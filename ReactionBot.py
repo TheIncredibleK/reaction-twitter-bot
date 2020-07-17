062f97cc18f646129c183b3e3dc2192ac3b61635
Keith Kenny <keith.kny@gmail.com>
2020-07-17 18:22:12 +0100

    Init

---
+++ ReactionBot.py
@@ -0,0 +1,26 @@
+import os
+
+
+## Twitter Helpers ##
+def process_tweet(tweet):
+    return
+
+def reply_with_image(tweet, image):
+    return
+
+
+
+
+
+
+
+
+def main_loop():
+    while 1:
+        return
+
+
+
+
+if __name__ == "__main__":
+    main_loop()
